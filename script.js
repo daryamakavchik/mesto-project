15191541fa5d37c82ca15657e5f268ebcdbd3332
Darya Makavchik <darjamakavchik@gmail.com>
2021-12-02 09:29:15 +0300

    create add form

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let editButton = document.querySelector('.profile__edit-button');
 let saveButton = document.querySelector('.form__button');
-let display = document.querySelector('.popup_closed');
+let displayForm = document.querySelector('.popup_closed');
 
 function openEditForm () {
-  display.classList.remove('popup_closed');
+  displayForm.classList.remove('popup_closed');
 }
 editButton.addEventListener('click', openEditForm);
 
@@ -24,3 +24,17 @@ function closeEditForm () {
   display.classList.add('popup_closed');
 }
 closeButton.addEventListener('click', closeEditForm);
+
+
+let addButton = document.querySelector('.profile__add-button');
+let displayAddCard = document.querySelector('.popup__add-card');
+function openAddForm () {
+  displayAddCard.classList.remove('popup__add-card_closed');
+}
+addButton.addEventListener('click', openAddForm);
+
+let closeAddCardButton = document.querySelector('.popup__add-card-close-button');
+function closeAddCardForm () {
+  displayAddCard.classList.add('popup__add-card_closed');
+}
+closeAddCardButton.addEventListener('click', closeAddCardForm);
