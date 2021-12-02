97f37ce0866c0527882fb2795e1eb621b15b481a
Darya Makavchik <darjamakavchik@gmail.com>
2021-12-02 13:49:31 +0300

    add card feature

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+/*           OPEN FORM          */
 let editButton = document.querySelector('.profile__edit-button');
-let saveButton = document.querySelector('.form__button');
 let displayForm = document.querySelector('.popup_closed');
-
 function openEditForm () {
   displayForm.classList.remove('popup_closed');
 }
 editButton.addEventListener('click', openEditForm);
 
+/*           EDIT AND SAVE FORM         */
+let saveButton = document.querySelector('.form__button');
 const formInput = document.querySelector('.form');
 const userName = document.querySelector('#username');
 const userNameInfo = document.querySelector('#usernameinfo');
@@ -18,7 +19,7 @@ function formSubmitHandler (evt) {
   formInput.addEventListener('submit', formSubmitHandler);
   saveButton.addEventListener('click', closeEditForm);
 
-
+/*           CLOSE FORM         */
 let closeButton = document.querySelector('.popup__close-button');
 function closeEditForm () {
   display.classList.add('popup_closed');
@@ -26,6 +27,7 @@ function closeEditForm () {
 closeButton.addEventListener('click', closeEditForm);
 
 
+/*          OPEN ADD-CARD FORM          */
 let addButton = document.querySelector('.profile__add-button');
 let displayAddCard = document.querySelector('.popup__add-card');
 function openAddForm () {
@@ -33,8 +35,31 @@ function openAddForm () {
 }
 addButton.addEventListener('click', openAddForm);
 
+
+/*          EDIT AND SAVE ADD-CARD FORM          */
+let saveAddCardButton = document.querySelector('#addcardbutton'); /*addbutton*/
+const addFormInput = document.querySelector('#addform'); /*addform*/  
+const placeName = document.querySelector('#placename'); /*input placename*/
+const imageLink = document.querySelector('#imagelink'); /*input link*/
+const cardTemplate = document.querySelector('#cardtemplate').content; /*template content*/
+const elements = document.querySelector('.elements'); /*where to place template*/
+const cardElement = elements.querySelector('.elements__element').cloneNode(true); /*copy template content*/
+
+function addFormSubmitHandler (evt) {
+  evt.preventDefault(); 
+  cardElement.querySelector('.elements__image').src = imageLink.value; /*source of image of a new card = input link*/
+  cardElement.querySelector('.elements__text').textContent = placeName.value; /*text of a new card = input text*/
+  elements.prepend(cardElement); /*add template content to elements*/ 
+}
+  addFormInput.addEventListener('submit', addFormSubmitHandler); /*submit addform and insert link+image*/
+  saveAddCardButton.addEventListener('click', closeAddCardForm); /*close addform*/
+
+/*           CLOSE ADD-CARD FORM          */
 let closeAddCardButton = document.querySelector('.popup__add-card-close-button');
 function closeAddCardForm () {
   displayAddCard.classList.add('popup__add-card_closed');
 }
 closeAddCardButton.addEventListener('click', closeAddCardForm);
+
+
+
