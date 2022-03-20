af83379b46d2765f937d18d4f1915e0339b7edb0
Darya Makavchik <darjamakavchik@gmail.com>
2022-03-20 09:37:49 -0400

    second review fix

--- src/components/modal.js
+++ src/components/modal.js
@@ -11,6 +11,8 @@ const placeInput = document.querySelector("#placename");
 const profilePopup = document.querySelector(".popup__profile");
 const addCardPopup = document.querySelector(".popup__add-card");
 const imagePopup = document.querySelector(".popup-image");
+const cardSubmitButton = document.querySelector("#addcardbutton");
+const addCardForm = document.querySelector("#addcardform");
 
 function openProfilePopup() {
   userName.value = profileTitle.textContent;
@@ -33,15 +35,11 @@ function handleAddCardFormSubmit(evt) {
   evt.preventDefault();
   const name = placeInput.value;
   const link = `${imageInput.value}`;
-  const saveCardButton = document.querySelector('.form__button-submit');
   elements.prepend(createCard(name, link));
-  imageInput.value = "";
-  placeInput.value = "";
-  if (imageInput.length < 2 && placeInput.length < 2) {
-    saveCardButton.classList.add('form__button-submit_disabled');
-    saveCardButton.disabled = true;
-  }
   closePopup(addCardPopup);
+  addCardForm.reset();
+  cardSubmitButton.classList.add('form__button-submit_disabled');
+  cardSubmitButton.disabled = true;
 }
 
 function openImagePopup() {
