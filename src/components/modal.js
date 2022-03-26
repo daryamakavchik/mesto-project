355ffbef0480b11512e648768b56de0366f65ad3
Darya Makavchik <darjamakavchik@gmail.com>
2022-03-26 13:25:58 -0400

    fix: rendererror

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,29 +1,36 @@
 import { openPopup, closePopup } from "./utils.js";
-import { createCard } from "./card.js";
+import { saveNewProfilePic, addNewCard, setUserInfo } from "./app.js";
 
-const elements = document.querySelector(".elements");
 const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
-const userName = document.querySelector("#username");
-const userNameInfo = document.querySelector("#usernameinfo");
+const username = document.querySelector("#username");
+const usernameInfo = document.querySelector("#usernameinfo");
 const imageInput = document.querySelector("#imagelink");
 const placeInput = document.querySelector("#placename");
+const pictureLink = document.querySelector("#profilepicture");
 const profilePopup = document.querySelector(".popup__profile");
 const addCardPopup = document.querySelector(".popup__add-card");
 const imagePopup = document.querySelector(".popup-image");
+const profileSubmitButton = document.querySelector("#profilesubmitbutton");
 const cardSubmitButton = document.querySelector("#addcardbutton");
+const profilePicSubmitButton = document.querySelector(
+  "#profilepicsubmitbutton"
+);
 const addCardForm = document.querySelector("#addcardform");
+const profilePicPopup = document.querySelector(".popup-profilepic");
 
 function openProfilePopup() {
-  userName.value = profileTitle.textContent;
-  userNameInfo.value = profileSubtitle.textContent;
+  username.value = profileTitle.textContent;
+  usernameInfo.value = profileSubtitle.textContent;
   openPopup(profilePopup);
 }
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  profileTitle.textContent = userName.value;
-  profileSubtitle.textContent = userNameInfo.value;
+  renderLoading(true, profileSubmitButton);
+  profileTitle.textContent = username.value;
+  profileSubtitle.textContent = usernameInfo.value;
+  setUserInfo(username.value, usernameInfo.value);
   closePopup(profilePopup);
 }
 
@@ -33,17 +40,37 @@ function openAddCardPopup() {
 
 function handleAddCardFormSubmit(evt) {
   evt.preventDefault();
-  const name = placeInput.value;
-  const link = `${imageInput.value}`;
-  elements.prepend(createCard(name, link));
+  renderLoading(true, cardSubmitButton);
+  addNewCard(placeInput.value, imageInput.value);
   closePopup(addCardPopup);
   addCardForm.reset();
-  cardSubmitButton.classList.add('form__button-submit_disabled');
+  cardSubmitButton.classList.add("form__button-submit_disabled");
   cardSubmitButton.disabled = true;
+}
+
+function handleEditProfilePic(evt) {
+  evt.preventDefault();
+  renderLoading(true, profilePicSubmitButton);
+  saveNewProfilePic(pictureLink.value);
+  closePopup(profilePicPopup);
 }
 
 function openImagePopup() {
   openPopup(imagePopup);
+}
+
+function openEditProfilePic() {
+  openPopup(profilePicPopup);
+}
+
+function renderLoading(isLoading, someButton) {
+  if (isLoading) {
+    someButton.textContent = "Сохранение...";
+  } else if (someButton === cardSubmitButton) {
+    someButton.textContent = "Создать";
+  } else {
+    someButton.textContent = "Сохранить";
+  }
 }
 
 export {
@@ -52,4 +79,14 @@ export {
   openAddCardPopup,
   handleAddCardFormSubmit,
   openImagePopup,
+  openEditProfilePic,
+  handleEditProfilePic,
+  renderLoading,
+  profileSubmitButton,
+  cardSubmitButton,
+  profilePicSubmitButton,
+  username,
+  usernameInfo,
+  profileTitle,
+  profileSubtitle,
 };
